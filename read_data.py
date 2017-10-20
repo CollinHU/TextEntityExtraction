507c1142ae25fbdf743b77f2b47d653c66140a7a
Ubuntu <ubuntu@ip-172-31-11-143.us-west-2.compute.internal>
2017-10-20 16:35:06 +0000

    processed data

--- read_data.py
+++ read_data.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
+import csv
+import re
 
-data = pd.read_csv('test.csv')
-print data.count()
-comments = data['comment'].apply(lambda s: np.nan if s[:3] == '???' or len(s) == 1 else s)
-comments.dropna(inplace = True)
-print comments.head(20)
+data = pd.read_csv('test.csv',quoting=csv.QUOTE_ALL)
+#print data.count()
+data['comment']= data['comment'].apply(lambda s: np.nan if s[:3] == '???' or len(s) == 1 else s)
+#print type(comments)
+data.dropna(inplace = True)
+data.reset_index(inplace=True)
+
+comments = data['comment']
+#print comments.head(20)
 print(comments.count())
-
-comments.to_csv('data.csv',index = False)
+comments = comments.apply(lambda s: re.sub("\"",'',s))
+comments = comments.apply(lambda s: re.sub('\n',' ',s))
+comments = comments.apply(lambda s: re.sub('\s+',' ',s))
+comments.to_csv('data.csv')
